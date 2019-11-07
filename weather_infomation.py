dbac8c1a625bc6ed15b110db5d2f2c9fbfbec7ca
Takayoshi <a>
2019-11-08 00:33:11 +0900

    Q2 途中

---
+++ weather_infomation.py
@@ -0,0 +1,20 @@
+weather_information = [
+    {'prefecture': '東京都', 'station': '渋谷', 'temperature': 6.5},
+    {'prefecture': '東京都', 'station': '池袋', 'temperature': 7.0},
+    {'prefecture': '東京都', 'station': '新橋', 'temperature': 7.5},
+
+    {'prefecture': '大阪府', 'station': '梅田', 'temperature': 8.2},
+    {'prefecture': '大阪府', 'station': '大阪', 'temperature': 9.3},
+    {'prefecture': '大阪府', 'station': '堺', 'temperature': 9.5},
+
+    {'prefecture': '福岡県', 'station': '博多', 'temperature': 13.0},
+    {'prefecture': '福岡県', 'station': '太宰府', 'temperature': 15.0},
+    ]
+
+    # Q1. 全国の平均気温を計算してください(9.5となればOK)
+print(weather_information['temperature'])
+# Average_temperature = sum(weather_information['temperature']) / len(weather_information['temperature'])
+# print(Average_temperature)
+    # Q2. 大阪府のすべての駅名をカンマ区切りで出力してください( '梅田,大阪,堺' となればOK)
+
+    # Q3. 福岡県の平均気温を計算してください(14.0となればOK)
