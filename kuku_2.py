56ef33efe8f08f11ad276a8dd880bbb1da284549
Takayoshi <a>
2019-11-07 23:28:19 +0900

    任意の掛け算表完成!

--- kuku_2.py
+++ kuku_2.py
@@ -1,3 +1,10 @@
 # 下記のような出力が得られる kuku_2.py を実装してください
 # 任意の行数および任意の列数での掛け算の結果が得られます
 
+line = int(input('行数を入力してください: '))
+column = int(input('桁数を入力してください: '))
+
+for x in range(1, line + 1):
+    for y in range(1, column + 1):
+        print(x * y, end=' ')
+    print()
