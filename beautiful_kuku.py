07bc20c99ffd25d601b0e3fbbdad3a6e7d51086d
Takayoshi <a>
2019-11-08 18:50:24 +0900

    綺麗な九九表完成!

--- beautiful_kuku.py
+++ beautiful_kuku.py
@@ -1,38 +1,12 @@
-def main():
-    # 3都府県のいくつかの駅名とある日の最高気温(単位: ℃)のデータを辞書として持っています
-    weather_information = [
-        {'prefecture': '東京都', 'station': '渋谷', 'temperature': 6.5},
-        {'prefecture': '東京都', 'station': '池袋', 'temperature': 7.0},
-        {'prefecture': '東京都', 'station': '新橋', 'temperature': 7.5},
+# きれいに整っているので見やすくなっています
+# 式が表示されている
+# 結果の桁数が違う場合は適切な量の半角スペースを挿入しているので、みやすい
+# ※結果が3桁の場合は崩れてもOKです
 
-        {'prefecture': '大阪府', 'station': '梅田', 'temperature': 8.2},
-        {'prefecture': '大阪府', 'station': '大阪', 'temperature': 9.3},
-        {'prefecture': '大阪府', 'station': '堺', 'temperature': 9.5},
+line = int(input('行数を入力してください: '))
+column = int(input('桁数を入力してください: '))
 
-        {'prefecture': '福岡県', 'station': '博多', 'temperature': 13.0},
-        {'prefecture': '福岡県', 'station': '太宰府', 'temperature': 15.0},
-    ]
-
-    # Q1. 全国の平均気温を計算してください(9.5となればOK)
-    average_temperature = 0
-    for Tempetatrues in weather_information:
-        average_temperature += Tempetatrues['temperature']
-    print(f'{average_temperature / len(weather_information)}')
-
-    # Q2. 大阪府のすべての駅名をカンマ区切りで出力してください( '梅田,大阪,堺' となればOK)
-    osaka =list()
-    for Station in weather_information:
-        if '大阪府' == Station['prefecture']:
-            osaka.append(Station['station'])
-    print(','.join(osaka))
-
-    # Q3. 福岡県の平均気温を計算してください(14.0となればOK)
-    temps = []
-    for tempetatrues in weather_information:
-        if tempetatrues['prefecture'] == '福岡県':
-            temps.append(tempetatrues['temperature'])
-    print(sum(temps) / len(temps))
-
-
-if __name__ == '__main__':
-    main()
+for x in range(1, line + 1):
+    for y in range(1, column + 1):
+        print(f'{x:2} ×{y:2} ={x * y:2} |',end=' ')
+    print()
